ed033f0c8707c26cc1f3477b9b2f3199102b2a1d
Kinjal_Patil <patilkinjal312@gmail.com>
2022-07-04 14:01:20 +0530

    Updated models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from myapp.models import *
+
+admin.site.register(Project)
+admin.site.register(Tower)
+admin.site.register(Floor)
+admin.site.register(Flat)
+admin.site.register(Customer)
--- myapp/models.py
+++ myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    project_id = models.IntegerField(primary_key=True)
+    project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=500)
     description = models.TextField(null=True, blank=True)
     address = models.TextField(null=True, blank=True)
@@ -14,22 +14,23 @@ class Project(models.Model):
 
 
 class Tower(models.Model):
-    tower_id = models.IntegerField(primary_key=True)
+    tower_id = models.AutoField(primary_key=True)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
     tower_name = models.CharField(max_length=500)
     no_of_floors = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return self.tower_name
+        return str(self.project_id.project_name) + "->" + str(self.tower_name)
 
 
 class Floor(models.Model):
-    floor_id = models.IntegerField(primary_key=True)
+    floor_id = models.AutoField(primary_key=True)
     tower_id = models.ForeignKey(Tower, on_delete=models.CASCADE)
+    floor_no = models.IntegerField(null=True, blank=True)
     no_of_flats = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return self.floor_id
+        return str(self.tower_id.tower_name) + " -> " + str(self.floor_no)
 
 
 STATUS = (
@@ -41,22 +42,23 @@ STATUS = (
 
 
 class Flat(models.Model):
-    flat_id = models.IntegerField(primary_key=True)
+    flat_id = models.AutoField(primary_key=True)
     floor_id = models.ForeignKey(Floor, on_delete=models.CASCADE)
+    flat_no = models.IntegerField(null=True, blank=True)
     no_of_bedrooms = models.IntegerField(null=True, blank=True)
     floor_space = models.CharField(max_length=250, null=True, blank=True)
     no_of_balconies = models.IntegerField(null=True, blank=True)
     balconies_space = models.CharField(max_length=250, null=True, blank=True)
     status = models.CharField(choices=STATUS, max_length=250)
     description = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
+    price = models.CharField(max_length=250, null=True, blank=True)
 
     def __str__(self):
-        return self.flat_id
+        return str(self.floor_id) + "->" + str(self.flat_no)
 
 
 class Customer(models.Model):
-    cust_id = models.IntegerField(primary_key=True)
+    cust_id = models.AutoField(primary_key=True)
     cust_name = models.CharField(max_length=250, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     cust_phone = models.CharField(max_length=12, null=True, blank=True)
